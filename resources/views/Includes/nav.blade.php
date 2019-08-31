3d8eb7594a7d1054811a915d5f2d67adef66dfb8
Robert Bender <robertbender@Roberts-MacBook-Pro.local>
2019-08-31 15:27:45 -0700

    Added nav view

---
+++ resources/views/Includes/nav.blade.php
@@ -0,0 +1,17 @@
+ <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a href="#" class="navbar-brand">Academind</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Users</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Products</a>
+                </li>
+            </ul>
+        </div>
+
+    </nav>

4c7331d6de1318830b95e6aa98344dffaa87fab4
Robert Bender <robertbender@Roberts-MacBook-Pro.local>
2019-09-03 17:23:41 -0700

    Updated root pub path

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,8 +57,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
-            // 'root' => storage_path('app/public'),
+            // 'root' => public_path(),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],

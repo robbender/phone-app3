fb51376d99ec067d903b5f493290f707eb6db0a8
Robert Bender <robertbender@Roberts-MacBook-Pro.local>
2019-09-04 09:51:53 -0700

    Added enctype on form

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -191,22 +191,13 @@
                                         name="phone" placeholder="Phone" value="{{ $contact->phone }}">
                                 </div>
                             </div>
-
-                            {{-- <h6>Image upload</h6>
-                                {!! Form::open(['action' => 'ContactController@store', 'method' => 'POST', 'enctype'=>'multipart/form-data']) !!}
-                                    @method('PATCH')
-                                    @csrf
-                                    <div class="form-group">
-                                        {{Form::file('cover_image')}}
-                                    </div>
-                                {!! Form::close() !!} --}}
-                            {{-- <div class="">
-                                    <br>
-                                    <label>Image</label>
-                                    <input type="file"
-                                        class="form-control {{ $errors->has('image') ? 'is-invalid' : '' }} required"
-                                        name="image" enctype="multipart/form-data" accept=".jpg, .jpeg, .png" placeholder="Image">
-                                </div> --}}
+                            <div class="">
+                                <br>
+                                <label>Image</label>
+                                <input type="file"
+                                    class="form-control {{ $errors->has('image') ? 'is-invalid' : '' }} required"
+                                    name="image" accept=".jpg, .jpeg, .png" placeholder="Image">
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
@@ -271,7 +262,7 @@
                     </div>
                     <div class="modal-body">
                             <br>
-                            <form method="POST" action="/contacts">
+                            <form method="POST" action="/contacts" enctype="multipart/form-data">
                                 @csrf
                                 <div class="field">
                                     <label class="label" for="name">Name</label>
@@ -296,7 +287,7 @@
                                         <label>Image</label>
                                         <input type="file"
                                             class="form-control {{ $errors->has('image') ? 'is-invalid' : '' }} required"
-                                            name="image" enctype="multipart/form-data" accept=".jpg, .jpeg, .png" placeholder="Image" required>
+                                            name="image" enctype="multipart/form-data" accept=".jpg, .jpeg, .png" placeholder="Image">
                                     </div>
                                 <br>
 
